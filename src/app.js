0c2c2c6fe82bf491bebc4f91f4ec59ed9cd9cc20
CodeWith-Arsalan <arsalanshahid6655@gmail.com>
2024-07-27 14:23:43 +0300

    Setup the app for heroku but its paid so didn't change the package.json and only change the port

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -123,7 +125,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up and running")
+app.listen(port, () => {
+    console.log("Server is up and running"+ port)
 })
 
